f259a3d930345f5d24d7b7e27ca7bb09788bd81c
Stefan Ghioci <ghioci.stefan@gmail.com>
2020-03-06 01:34:45 +0200

    Rework cluster centering function

--- src/ai/impl/BlockMappingClustering.java
+++ src/ai/impl/BlockMappingClustering.java
@@ -3,7 +3,7 @@ package ai.impl;
 import ai.KMeansClustering;
 import model.BlockMapping;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -21,22 +21,26 @@ public class BlockMappingClustering extends KMeansClustering<BlockMapping>
         for (int x = 0; x < BLOCK_SIZE; x++)
             for (int y = 0; y < BLOCK_SIZE; y++)
             {
-                double avgValue = 0;
+//                double avgValue = 0;
+//                for (BlockMapping blockMapping : cluster)
+//                    avgValue = avgValue + blockMapping.getMapping()[x][y];
+//                avgValue /= cluster.size();
+//
+//                mapping[x][y] = Math.toIntExact(Math.round(avgValue));
+
+                List<Integer> values = new ArrayList<>();
                 for (BlockMapping blockMapping : cluster)
-                    avgValue = avgValue + blockMapping.getMapping()[x][y];
-                avgValue /= cluster.size();
-
-                mapping[x][y] = Math.toIntExact(Math.round(avgValue));
+                    values.add(blockMapping.getMapping()[x][y]);
 
 
-//                // noinspection OptionalGetWithoutIsPresent
-//                mapping[x][y] = values.stream()
-//                                      .collect(Collectors.groupingBy(w -> w, Collectors.counting()))
-//                                      .entrySet()
-//                                      .stream()
-//                                      .max(Map.Entry.comparingByValue())
-//                                      .get()
-//                                      .getKey();
+                // noinspection OptionalGetWithoutIsPresent
+                mapping[x][y] = values.stream()
+                                      .collect(Collectors.groupingBy(w -> w, Collectors.counting()))
+                                      .entrySet()
+                                      .stream()
+                                      .max(Map.Entry.comparingByValue())
+                                      .get()
+                                      .getKey();
             }
 
         return new BlockMapping(null, null, mapping, null);
